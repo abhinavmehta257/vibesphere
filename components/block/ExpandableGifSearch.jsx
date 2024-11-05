9204d2c993de7bbf3baebb4f3a4490e574442b51
Abhinav Mehta <abhinavmehta257@gmail.com>
2024-11-06 00:12:02 +0530

    gif list size changes

--- components/block/ExpandableGifSearch.jsx
+++ components/block/ExpandableGifSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { GiphyFetch } from "@giphy/js-fetch-api";
 import {
   Grid, // our UI Component to display the results
@@ -8,26 +8,28 @@ import {
   SuggestionBar, // an optional UI component that displays trending searches and channel / username results
 } from "@giphy/react-components";
 
-const SearchExperience = ({ setSelectedGif, setShowGifPicker }) => (
-  <SearchContextManager apiKey={"4PtUzZnbzuxWTLFRFZ64ClwzZitO6U1w"}>
-    <ExpandableGifSearch
-      setSelectedGif={setSelectedGif}
-      setShowGifPicker={setShowGifPicker}
-    />
-  </SearchContextManager>
-);
+const SearchExperience = ({ setSelectedGif, setShowGifPicker }) => {
+  return (
+    <SearchContextManager apiKey={"4PtUzZnbzuxWTLFRFZ64ClwzZitO6U1w"}>
+      <ExpandableGifSearch
+        setSelectedGif={setSelectedGif}
+        setShowGifPicker={setShowGifPicker}
+      />
+    </SearchContextManager>
+  );
+};
 
 const ExpandableGifSearch = ({ setSelectedGif, setShowGifPicker }) => {
   const { fetchGifs, searchKey } = useContext(SearchContext);
-
   const handleGifSelect = (gif) => {
     console.log("Selected GIF URL:", gif.images.original.url);
 
     setSelectedGif(gif.images.original.url); // Get the original URL of the selected GIF
     setShowGifPicker(false); // Close the GIF picker
   };
+
   return (
-    <div className="max-h-[300px]">
+    <div className="max-h-[250px]">
       <SearchBar />
       <SuggestionBar />
       <Grid
@@ -38,10 +40,9 @@ const ExpandableGifSearch = ({ setSelectedGif, setShowGifPicker }) => {
         noResultsMessage="No GIFs found"
         key={searchKey}
         searchPlaceholder="Search GIFs..." // Optional: Search placeholder
-        columns={3}
-        className="w-full h-[300px] overflow-scroll "
-        width={300}
-        height={300}
+        columns={4}
+        className="w-full h-[200px] overflow-y-auto "
+        width={250}
         noLink={true}
         hideAttribution={true}
       />
